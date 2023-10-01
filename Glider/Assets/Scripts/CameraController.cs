1e66dcd1e5c7c206f3ca62af916f2ff1bd9fa4cb
Filippo Santarelli <fsanta328@gmail.com>
2023-10-01 15:27:39 +0100

    Added camera controller to track glider.

---
+++ Glider/Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    class CameraController : MonoBehaviour
+    {
+        public Transform targetGlider;
+
+        public void Update()
+        {
+            Vector3 pos = new Vector3(targetGlider.position.x, transform.position.y, transform.position.z);
+            transform.position = pos;
+        }
+    }
+}
